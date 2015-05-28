81af588ede1f2696fde4c56f813d9e026c9f044d
Duncan Neal <duncan.neal@gmail.com>
2015-05-28 15:54:07 -0400

    Done.

--- pig_latin.rb
+++ pig_latin.rb
@@ -2,36 +2,30 @@ class PigLatin
 
   def self.translation(word)
     caps = word =~ /^[A-Z]/
+    punct = word =~ /[^\w\s\d]/
     length = word.index(/[aeiou]/i)
+    if punct
+      end_mark = word[-1]
+      word.gsub!(/[^\w\s\d]/, '')
+    end
     if length
       prefix = word.slice(0, length)
       suffix = length == 0 ? 'yay' : prefix + 'ay'
       suffix.downcase!
       new_word = word[length..-1] + suffix
       new_word.capitalize! if caps
+    if punct
+        new_word.insert(-1, end_mark).split
+    end
       new_word
     else
       word    
     end
-end
-
+  end
 
   def self.translate(word)
     words = word.split
     words.map! {|word| PigLatin.translation(word)}
     done = words.join(" ")
-  end
-
-  # def self.capital_translate(word)
-  #   words = word.split
-  #   words.map! {|word| PigLatin.translation(word)}
-  #   done = words.join(" ").downcase
-  #   final = done.split.each{|i| i.capitalize!}.join(" ")
-  # end
-
-  # def self.capital_first_word_only_translate(word)
-  #   words = word.split
-  #   words.map! {|word| PigLatin.translation(word)}
-  #   done = words.join(" ").capitalize
-  # end  
+  end 
 end
