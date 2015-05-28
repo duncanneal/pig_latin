414e7d8324a14e2019aa3689a984e085040c66f9
Duncan Neal <duncan.neal@gmail.com>
2015-05-28 15:10:11 -0400

    One test left (punctuation)

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,11 +1,18 @@
 class PigLatin
 
   def self.translation(word)
-    length = word.index(/[aeiou]/)
-    prefix = word.slice(0, length)
-    suffix = length == 0 ? 'yay' : prefix + 'ay'
-
-    word[length..-1] + suffix
+    caps = word =~ /^[A-Z]/
+    length = word.index(/[aeiou]/i)
+    if length
+      prefix = word.slice(0, length)
+      suffix = length == 0 ? 'yay' : prefix + 'ay'
+      suffix.downcase!
+      new_word = word[length..-1] + suffix
+      new_word.capitalize! if caps
+      new_word
+    else
+      word    
+    end
 end
 
 
@@ -15,17 +22,16 @@ end
     done = words.join(" ")
   end
 
-  def self.capital_translate(word)
-    words = word.split
-    words.map! {|word| PigLatin.translation(word)}
-    done = words.join(" ").downcase
-    final = done.split.each{|i| i.capitalize!}.join(" ")
-  end
+  # def self.capital_translate(word)
+  #   words = word.split
+  #   words.map! {|word| PigLatin.translation(word)}
+  #   done = words.join(" ").downcase
+  #   final = done.split.each{|i| i.capitalize!}.join(" ")
+  # end
 
-  def self.capital_first_word_only_translate(word)
-    words = word.split
-    words.map! {|word| PigLatin.translation(word)}
-    done = words.join(" ").capitalize
-  end  
-
+  # def self.capital_first_word_only_translate(word)
+  #   words = word.split
+  #   words.map! {|word| PigLatin.translation(word)}
+  #   done = words.join(" ").capitalize
+  # end  
 end
--- test/test_pig_latin.rb
+++ test/test_pig_latin.rb
@@ -29,16 +29,16 @@ class TestPigLatin < MiniTest::Test
   
   def test_05_The_Resemblance_Mutters
     expected = "Ethay Esemblanceray Uttersmay"
-    assert_equal expected, PigLatin.capital_translate("The Resemblance Mutters")
+    assert_equal expected, PigLatin.translate("The Resemblance Mutters")
   end 
 
   def test_06_15_Dangerous_Facts_Electricians_Keep_To_Themselves
     expected = "15 Angerousday Actsfay Electriciansyay Eepkay Otay Emselvesthay"
-    assert_equal expected, PigLatin.capital_translate("15 Dangerous Facts Electricians Keep To Themselves")
+    assert_equal expected, PigLatin.translate("15 Dangerous Facts Electricians Keep To Themselves")
   end
 
   def test_07_The_unpopular_glory_renames_an_ice!
     expected = "Ethay unpopularyay oryglay enamesray anyay iceyay!"
-    assert_equal expected, PigLatin.capital_first_word_only_translate("The unpopular glory renames an ice!")
+    assert_equal expected, PigLatin.translate("The unpopular glory renames an ice!")
   end             
 end
